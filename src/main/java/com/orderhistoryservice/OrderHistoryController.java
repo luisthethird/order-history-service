c2b49845187578e8023ef3f1602f6da05e102bd6
Luisito Nicasio <luisito@lusito.net>
2016-04-13 16:53:18 -0400

    Added GET and produces json

--- src/main/java/com/orderhistoryservice/OrderHistoryController.java
+++ src/main/java/com/orderhistoryservice/OrderHistoryController.java
@@ -1,14 +1,15 @@
 package hello;
 
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class OrderHistoryController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @RequestMapping(value = "/orderhistory", method = RequestMethod.GET, produces = "application/json")
+    public String orderhistory() {
+        return "orderhistory";
     }
 
 }
